f3c2761ef55632a29a41d08647920185940af36a
Connor Marble <connor.marble@gmail.com>
2016-02-22 16:24:39 +0000

    nitrous-backup at Mon Feb 22 16:24:39 UTC 2016

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,17 +5,13 @@ class Dog
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
+
 
   def name
     @name
   end
 
-def breed=(breed)
-    @breed = breed
-  end
+
 
   def breed
     @breed
